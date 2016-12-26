7445c5902d2a75af24ac8f3a21e4f9067ca35e80
vasanth1988 <vasanth1988@gmail.com>
2016-12-26 18:14:16 +0530

    Updated Startup batch

--- Startup.bat
+++ Startup.bat
@@ -1,3 +1,3 @@
 "%ProgramFiles%\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "CSGPortal" 12.0 -s
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%TF_BUILD_BINARIESDIRECTORY%\Snapshots\MT.CSGPortal.DBPackage.dacpac" /Profile:"%TF_BUILD_SOURCESDIRECTORY%\MT.CSGPortal.DBPackage.publish.xml"
+"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%TF_BUILD_BINARIESDIRECTORY%\MT.CSGPortal.DBPackage\MT.CSGPortal.DBPackage\Snapshots\MT.CSGPortal.DBPackage.dacpac" /Profile:"%TF_BUILD_SOURCESDIRECTORY%\MT.CSGPortal.DBPackage\MT.CSGPortal.DBPackage\MT.CSGPortal.DBPackage.publish.xml"
